e22ed6ad84f6b562333c5b397d3199755e3635e6
Darko Hristovski <hristovski15@gmail.com>
2024-05-06 21:50:46 +0200

    creating header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,38 @@
-const Header = () =>{
-    
+import { NavLink } from 'react-router-dom';
+import './header.css'
 
-    return <>
-    <p>ova e Header</p>
-    </>
-}
+const Header = () => {
+  return (
+    <header>
 
-export default Header
+      <div className="container">
+        <div className="wrapper">
+          <div className="display-flex space-between aNavLinkgn-items">
+            <a href="/" className='logo'>Logo</a>
+            <nav>
+              <ul className="display-flex space-between">
+                <li>
+                  <NavLink to="/" className={({isActive, isPending})=> isPending ? 'pending' : isActive ? 'active' : ''}>Home</NavLink>
+                </li>
+                <li>
+                <NavLink to="/product" className={({isActive, isPending})=> isPending ? 'pending' : isActive ? 'active' : ''}>Products</NavLink>
+
+                </li>
+                <li>
+                <NavLink to="/categories" className={({isActive, isPending})=> isPending ? 'pending' : isActive ? 'active' : ''}>Categories</NavLink>
+
+                </li>
+                <li>
+                <NavLink to="/contact" className={({isActive, isPending})=> isPending ? 'pending' : isActive ? 'active' : ''}>Contact</NavLink>
+
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
